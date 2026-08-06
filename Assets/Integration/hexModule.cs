16b6929f45388bbaaf6ec91b4774f0320538f4af
VoLtRL <raphael.lecaille@gmail.com>
2026-08-06 15:32:42 +0200

    first commit

---
+++ Assets/Integration/hexModule.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hexModule : MonoBehaviour
+{
+    // Références à lier dans l'inspecteur Unity
+    public KMBombModule BombModule;
+    public TextMesh DisplayText;
+    public MeshRenderer DisplayBackground;
+    public KMSelectable[] Buttons;
+    public Color[] AvailableColors; // Liste des couleurs possibles
+
+    public Color CurrentColor { get; private set; } // Couleur actuelle affichée
+    public int correctButtonIndex;
+
+    private int count = 0;
+
+    void Start()
+    {
+        BombModule.OnActivate += OnActivate;
+
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            int buttonIndex = i;
+
+            Buttons[buttonIndex].OnInteract += delegate ()
+            {
+                OnButtonPress(buttonIndex);
+                return false;
+            };
+        }
+    }
+
+    void OnActivate()
+    {
+        CurrentColor = GenerateRandomHexColor();
+        DisplayColor();
+        GenerateButtonColors();
+    }
+
+    void DisplayColor()
+    {
+        DisplayText.text = "#" + ColorUtility.ToHtmlStringRGB(CurrentColor);
+    }
+
+    Color GenerateRandomHexColor()
+    {
+        return new Color(Random.value, Random.value, Random.value);
+    }
+
+    void GenerateButtonColors()
+    {
+        Color[] alreadyUsedColors = new Color[Buttons.Length];
+        correctButtonIndex = Random.Range(0, Buttons.Length);
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            if (i == correctButtonIndex)
+            {
+                MeshRenderer buttonRenderer = Buttons[i].GetComponent<MeshRenderer>();
+                buttonRenderer.material.color = CurrentColor;
+            }
+            else
+            {
+                Color randomColor;
+                do
+                {
+                    randomColor = AvailableColors[Random.Range(0, AvailableColors.Length)];
+                } while (randomColor == CurrentColor || System.Array.IndexOf(alreadyUsedColors, randomColor) != -1);
+
+                alreadyUsedColors[i] = randomColor;
+                MeshRenderer buttonRenderer = Buttons[i].GetComponent<MeshRenderer>();
+                buttonRenderer.material.color = randomColor;
+            }
+        }
+    }
+
+    void OnButtonPress(int index)
+    {
+        Buttons[index].AddInteractionPunch();
+        GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Buttons[index].transform);
+
+        MeshRenderer buttonRenderer = Buttons[index].GetComponent<MeshRenderer>();
+
+        if (index == correctButtonIndex)
+        {
+            Debug.Log("Correct ");
+            count++;
+            if (count >= 3)
+            {
+                BombModule.HandlePass();
+            }
+            else
+            {
+                CurrentColor = GenerateRandomHexColor();
+                DisplayColor();
+                GenerateButtonColors();
+            }
+        }
+        else
+        {
+            Debug.Log("Wrong ");
+            BombModule.HandleStrike();
+        }
+
+    }
+}
